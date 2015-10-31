31ba134f18312a7ac1d4f390b4c21be005824aa9
Johann Schleier-Smith <jssmith@gmail.com>
2015-10-31 15:44:06 -0700

    build with modules

---
+++ project/Build.scala
@@ -0,0 +1,42 @@
+import sbt._
+import sbt.Keys._
+
+object BuildSettings {
+  val buildSettings = Defaults.coreDefaultSettings ++ Seq(
+    organization := "replaydb",
+    version := "0.1-SNAPSHOT",
+    scalaVersion := "2.11.7",
+    scalacOptions ++= Seq(
+      "-feature",
+      "-language:implicitConversions",
+      "-language:experimental.macros",
+      "-deprecation"
+    )
+  )
+}
+
+object ReplayDBBuild extends Build {
+  import BuildSettings._
+
+  lazy val root = Project("root", file("."),
+    settings = buildSettings
+  ) aggregate(core, apps)
+
+  lazy val core = Project("core", file("core"),
+    settings = buildSettings ++ Seq(
+      name := "replaydb-core",
+      libraryDependencies ++= Seq(
+        "com.twitter" %% "chill" % "0.7.1",
+        "org.apache.commons" % "commons-math3" % "3.5",
+        "com.google.guava" % "guava" % "18.0",
+        "org.scalatest" %% "scalatest" % "2.2.5"
+      )
+    )
+  )
+
+  lazy val apps = Project("apps", file("apps"),
+    settings = buildSettings ++ Seq(
+      name := "replaydb-apps"
+    )
+  ) dependsOn (core)
+}
