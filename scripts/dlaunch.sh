ff99018d89dcb060061c2a00b28e0e7737f7278b
Johann Schleier-Smith <jssmith@gmail.com>
2015-12-05 22:21:43 +0000

    ec2 launch scripts

---
+++ scripts/dlaunch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+NUM=$1
+echo "Launching $NUM services"
+
+PORT_START=5567
+PORT_END=`expr $PORT_START + $NUM - 1`
+
+readarray WORKERS < /home/ec2-user/conf/workers.txt
+
+NUM_WORKERS=${#WORKERS[@]}
+
+rm /home/ec2-user/conf/latesthosts.txt
+
+for port in `seq $PORT_START $PORT_END`; do
+  HOSTINDEX=$((port%NUM_WORKERS))
+  HOST=${WORKERS[$HOSTINDEX]}
+  HOST=`echo $HOST`
+  echo "Launch on $HOST:$port"
+  ssh $HOST "bash /home/ec2-user/replaydb-worker/llaunch.sh $port"
+  echo "$HOST:$port" >> /home/ec2-user/conf/latesthosts.txt
+done
