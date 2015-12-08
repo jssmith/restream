bfb6b25a34e1c953b1219f4ed43c40c5e5534a11
Johann Schleier-Smith <jssmith@gmail.com>
2015-12-07 23:28:16 -0800

    smaller batch size

--- scripts/ddrive.sh
+++ scripts/ddrive.sh
@@ -10,5 +10,5 @@ time java -Xmx2000m \
   -cp /home/ec2-user/replaydb/apps/target/scala-2.11/replaydb-apps-assembly-0.1-SNAPSHOT.jar \
   replaydb.exec.spam.DistributedSpamDetector $SPAM_DETECTOR \
   /home/ec2-user/data/events-$SIZE_SPEC-split-$NUM/events.out \
-  $NUM 50000 /home/ec2-user/conf/latesthosts.txt
+  $NUM 10000 /home/ec2-user/conf/latesthosts.txt
 
