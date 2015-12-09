858622cb2d0a3de926c21df4603a59725283ae1e
Johann Schleier-Smith <jssmith@gmail.com>
2015-12-08 18:22:16 -0800

    more memory for workers

--- scripts/llaunch.sh
+++ scripts/llaunch.sh
@@ -3,4 +3,4 @@
 port=$1
 logfile="/home/ec2-user/log/$port"
 echo "logging to $logfile"
-nohup /home/ec2-user/jdk1.8.0_66/bin/java -Xmx3000m -Dreplaydb-logdir=/home/ec2-user/log -cp /home/ec2-user/replaydb-worker/replaydb-apps-assembly-0.1-SNAPSHOT.jar replaydb.exec.WorkerService $port > $logfile.launch-out 2> $logfile.launch-err < /dev/null &
+nohup /home/ec2-user/jdk1.8.0_66/bin/java -Xmx6000m -Dreplaydb-logdir=/home/ec2-user/log -cp /home/ec2-user/replaydb-worker/replaydb-apps-assembly-0.1-SNAPSHOT.jar replaydb.exec.WorkerService $port > $logfile.launch-out 2> $logfile.launch-err < /dev/null &
