e4da406656136cfb19b82e318694e355a4ee7d00
Johann Schleier-Smith <jssmith@gmail.com>
2016-01-28 16:14:02 -0800

    provide filename for event counter

--- core/src/main/scala/replaydb/exec/EventCounter.scala
+++ core/src/main/scala/replaydb/exec/EventCounter.scala
@@ -2,12 +2,21 @@ package replaydb.exec
 
 import java.io.{BufferedInputStream, FileInputStream}
 
+import replaydb.exec.EventGenerator._
 import replaydb.io.SocialNetworkStorage
 import replaydb.util.ProgressMeter
 
 object EventCounter extends App {
+  if (args.length != 1) {
+    println(
+      """Usage: EventCounter filename
+      """.stripMargin)
+    System.exit(1)
+  }
+  var filename = args(0)
+
   val eventStorage = new SocialNetworkStorage
   val pm = new ProgressMeter(printInterval = 1000000)
-  val r = eventStorage.readEvents(new BufferedInputStream(new FileInputStream("/tmp/events.out")), e => pm.increment())
+  val r = eventStorage.readEvents(new BufferedInputStream(new FileInputStream(filename)), e => pm.increment())
   pm.finished()
 }
