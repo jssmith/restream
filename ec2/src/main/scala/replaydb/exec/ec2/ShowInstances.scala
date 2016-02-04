dbce2e56613d7b7b4f2098857f7066bc417bc696
Erik Krogen <erikkrogen@gmail.com>
2016-02-03 23:13:19 -0800

    Improve printing of instances for ShowInstances, add ability to only print running instances

--- ec2/src/main/scala/replaydb/exec/ec2/ShowInstances.scala
+++ ec2/src/main/scala/replaydb/exec/ec2/ShowInstances.scala
@@ -10,28 +10,26 @@ import scala.collection.JavaConversions._
  * Show basic information about the instances
  */
 object ShowInstances extends App {
+  val showRunningOnly = args.length > 0 && args(0) == "true"
   val credentials = new ProfileCredentialsProvider()
   val ec2client = new AmazonEC2Client(credentials)
   ec2client.setEndpoint("ec2.us-west-1.amazonaws.com")
   val instances: DescribeInstancesResult = ec2client.describeInstances()
   for (x <- instances.getReservations) {
     val instances = x.getInstances
-    for (i <- instances) {
-      println(i.getInstanceId)
-      println(i.getImageId)
-      println(i.getHypervisor)
-      println(i.getVirtualizationType)
-      println(i.getKeyName)
+    for (i <- instances if !showRunningOnly || i.getState.getCode == 16) {
+      println(s"Instance / Image ID                  : ${i.getInstanceId} // ${i.getImageId}")
+      println(s"Hypervisor / Virt. Type / Inst. Type : ${i.getHypervisor} // ${i.getVirtualizationType} // ${i.getInstanceType}")
+      println(s"Key / Security Group                 : ${i.getKeyName} // ${i.getSecurityGroups}")
       val name = i.getTags.find("Name" == _.getKey) match {
         case Some(tag) => tag.getValue
         case None => "No name"
       }
-      println(name)
-      println(i.getInstanceType)
-      println(i.getSecurityGroups)
-      println(i.getEbsOptimized)
-      println(i.getState)
+      println(s"Name                                 : $name")
+      println(s"State                                : ${i.getState}")
       println("----------------")
     }
   }
+  println(s"Total of ${instances.getReservations.map(_.getInstances.length).sum} instances, " +
+    s"of which ${instances.getReservations.map(_.getInstances.count(_.getState.getCode == 16)).sum} are running")
 }
