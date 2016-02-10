75dfa0552bf54e3692c135537b19cab425620017
Erik Krogen <erikkrogen@gmail.com>
2016-02-09 23:57:09 -0800

    Add ability for master to generate (truncated) data for itself

--- scripts/dgendata.sh
+++ scripts/dgendata.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 #
-# Generate data on all workers 
+# Generate data on all workers, and locally (truncated to 1/50th the length) 
 # Usage: ./dgendata.sh size_spec num_events num_users split_nums
-#  e.g.: ./dgendata.sh 50m 50000000 100000 "1 2 4 8 16 32" -1
-#
-#        keep_only denotes the denotes the *only* output partition that should 
-#        actually be saved to disk (-1 for all). should be this host's partition ID
-#
+#  e.g.: ./dgendata.sh 50m 50000000 100000 "1 2 4 8 16 32"
 
 if [[ $# -ne 4 ]]; then
-  echo "Usage: ./dgendata.sh size_spec num_events num_users split_nums keep_only"
-  echo "e.g.: ./dgendata.sh 50m 50000000 100000 \"1 2 4 8 16 32\" -1"
-  echo "   keep_only denotes the denotes the *only* output partition that should "
-  echo "   actually be saved to disk (-1 for all). should be this host's partition ID"
+  echo "Usage: ./dgendata.sh size_spec num_events num_users split_nums"
+  echo "e.g.: ./dgendata.sh 50m 50000000 100000 \"1 2 4 8 16 32\""
   exit
 fi
 
+echo "generating data locally, $2 events"
+idx=0
 for host in `cat $HOME/conf/workers.txt`; do
-  ssh $host "bash -l -c '/home/ec2-user/replaydb-worker/lgendata.sh $1 $2 $3 \"$4\" $5'" &
+  ssh $host "bash -l -c '/home/ec2-user/replaydb-worker/lgendata.sh $1 $2 $3 \"$4\" $idx'" &
+  idx=$(($idx+1))
 done
+
+truncated_size=$(($2/50))
+echo "generating data locally, $truncated_size events"
+$HOME/replaydb-worker/lgendata.sh $1 $truncated_size $3 "$4" $5
--- scripts/distcode.sh
+++ scripts/distcode.sh
@@ -11,3 +11,9 @@ for host in `cat $HOME/conf/workers.txt`; do
     $host:replaydb-worker
 done
 
+# Also distribute to self so that commands expecting that code work on master as well 
+cp /home/ec2-user/replaydb/apps/target/scala-2.11/replaydb-apps-assembly-0.1-SNAPSHOT.jar \
+  $HOME/replaydb/scripts/llaunch.sh \
+  $HOME/replaydb/scripts/lgendata.sh \
+  $HOME/replaydb/scripts/setup_java_debug_opts.sh \
+    $HOME/replaydb-worker
