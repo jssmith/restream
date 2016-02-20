a8cf9d44d5e236612a8b811412d8a25fd8e82c39
Johann Schleier-Smith <jssmith@gmail.com>
2016-02-19 18:44:05 -0800

    reduce memory consumption on log processing

--- apps/src/main/scala/replaydb/exec/spam/runAnalysis/WorkerLogs.scala
+++ apps/src/main/scala/replaydb/exec/spam/runAnalysis/WorkerLogs.scala
@@ -26,27 +26,37 @@ class WorkerLogs(workerLogPath: File, runConfig: LoggedRunConfiguration) {
     val perfFiles = workerLogPath.listFiles(new FilenameFilter {
       override def accept(dir: File, name: String): Boolean = name.endsWith(".perf")
     })
-    val allContent = (for (f <- perfFiles) yield {
-      Util.readFully(f)
-    }).mkString("\n")
-    val readerThreadMs = findAllTimes(allContent, """reader thread time (\d+) ms""").sum
-    val bossThreadMs = findAllTimes(allContent, """boss thread pool time: (\d+) ms""").sum
-    val workerThreadMs = findAllTimes(allContent, """worker thread pool time: (\d+) ms""").sum
-    val phaseThreadMs = findAllTimes(allContent, """thread \d+ \(phase \d+\) finished with elapsed cpu time (\d+) ms""").sum
-    val kryoSendMs = findAllTimes(allContent, """sent_kryo_ms=(\d+)""").sum
-    val kryoRecvMs = findAllTimes(allContent, """recv_kryo_ms=(\d+)""").sum
-    val kryoSendBytes = findAllTimes(allContent, """sent_bytes=(\d+)""").sum
-    val kryoRecvBytes = findAllTimes(allContent, """recv_bytes=(\d+)""").sum
-    new PerfSummary(
-      readerThreadMs = readerThreadMs,
-      bossThreadMs = bossThreadMs,
-      workerThreadMs = workerThreadMs,
-      phaseThreadMs = phaseThreadMs,
-      kryoSendMs = kryoSendMs,
-      kryoRecvMs = kryoRecvMs,
-      kryoSendBytes = kryoSendBytes,
-      kryoRecvBytes = kryoRecvBytes
-    )
+    val allPerf = for (f <- perfFiles) yield {
+      val perfText = Util.readFully(f)
+      val readerThreadMs = findAllTimes(perfText, """reader thread time (\d+) ms""").sum
+      val bossThreadMs = findAllTimes(perfText, """boss thread pool time: (\d+) ms""").sum
+      val workerThreadMs = findAllTimes(perfText, """worker thread pool time: (\d+) ms""").sum
+      val phaseThreadMs = findAllTimes(perfText, """thread \d+ \(phase \d+\) finished with elapsed cpu time (\d+) ms""").sum
+      val kryoSendMs = findAllTimes(perfText, """sent_kryo_ms=(\d+)""").sum
+      val kryoRecvMs = findAllTimes(perfText, """recv_kryo_ms=(\d+)""").sum
+      val kryoSendBytes = findAllTimes(perfText, """sent_bytes=(\d+)""").sum
+      val kryoRecvBytes = findAllTimes(perfText, """recv_bytes=(\d+)""").sum
+      new PerfSummary(
+        readerThreadMs = readerThreadMs,
+        bossThreadMs = bossThreadMs,
+        workerThreadMs = workerThreadMs,
+        phaseThreadMs = phaseThreadMs,
+        kryoSendMs = kryoSendMs,
+        kryoRecvMs = kryoRecvMs,
+        kryoSendBytes = kryoSendBytes,
+        kryoRecvBytes = kryoRecvBytes
+      )
+    }
+    allPerf.reduce((p1: PerfSummary, p2:PerfSummary) => new PerfSummary(
+      readerThreadMs = p1.readerThreadMs + p2.readerThreadMs,
+      bossThreadMs = p1.bossThreadMs + p2.bossThreadMs,
+      workerThreadMs = p1.workerThreadMs + p2.workerThreadMs,
+      phaseThreadMs = p1.phaseThreadMs + p2.phaseThreadMs,
+      kryoSendMs = p1.kryoSendMs + p2.kryoSendMs,
+      kryoRecvMs = p1.kryoRecvMs + p2.kryoRecvMs,
+      kryoSendBytes = p1.kryoSendBytes + p2.kryoSendBytes,
+      kryoRecvBytes = p1.kryoRecvBytes + p2.kryoRecvBytes
+    ))
   }
   def checkErrors(): Unit = {
     val errFiles = workerLogPath.listFiles(new FilenameFilter {
