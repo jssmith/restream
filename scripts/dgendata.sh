3ebd5e0fb4f9f7bbb01c6497fd0c6f30902abf68
Erik Krogen <erikkrogen@gmail.com>
2016-02-24 11:25:45 -0800

    Adjust generation scripts to accept a partitioned parameter

--- scripts/dgendata.sh
+++ scripts/dgendata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Generate data on all workers, and locally (truncated to 1/50th the length) 
+# Generate data on all workers, and locally (truncated to 1/50th the length)
 # Usage: ./dgendata.sh size_spec num_events num_users split_nums [ keep_only=false ]
 #  e.g.: ./dgendata.sh 50m 50000000 100000 "1 2 4 8 16 32" false
 #
@@ -8,8 +8,8 @@
 #  note that this will only work if each host is running only one partition
 
 if [[ $# -lt 4 ]]; then
-  echo "Usage: ./dgendata.sh size_spec num_events num_users split_nums [ keep_only=false ]"
-  echo "e.g.: ./dgendata.sh 50m 50000000 100000 \"1 2 4 8 16 32\" false"
+  echo "Usage: ./dgendata.sh size_spec num_events num_users split_nums [ keep_only=false ] [ partitioned=false ]"
+  echo "e.g.: ./dgendata.sh 50m 50000000 100000 \"1 2 4 8 16 32\" false true"
   echo ""
   echo "if keep_only is true, each host will keep only the data relevant to its partition ID"
   echo "note that this will only work if each host is running only one partition"
@@ -21,6 +21,11 @@ if [ $# -ge 5 ]; then
 else
   keep_only=false
 fi
+if [ $# -ge 6 ]; then
+  partitioned=$6
+else
+  partitioned=false
+fi
 
 echo "generating data locally, $2 events"
 idx=0
@@ -30,7 +35,7 @@ for host in `cat $HOME/conf/workers.txt`; do
   else
     keep_only_arg=-1
   fi
-  ssh $host "bash -l -c '/home/ec2-user/replaydb-worker/lgendata.sh $1 $2 $3 \"$4\" $keep_only_arg'" &
+  ssh $host "bash -l -c '/home/ec2-user/replaydb-worker/lgendata.sh $1 $2 $3 \"$4\" $keep_only_arg $partitioned'" &
   idx=$(($idx+1))
 done
 
--- scripts/lgendata_scaledsize.sh
+++ scripts/lgendata_scaledsize.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #
-# Locally generate data 
+# Locally generate data
 # Note that in this case, num_events should be the number of events *per split*
 # Usage: ./lgendata_scaledsize.sh size_spec num_events num_users split_nums [ keep_only=-1 ]
 #  e.g.: ./lgendata_scaledsize.sh 5m-each 5000000 100000 "1 2 4 8 16 32" 1
-#        
-#        keep_only denotes the denotes the *only* output partition that should 
+#
+#        keep_only denotes the denotes the *only* output partition that should
 #        actually be saved to disk (-1 for all). should be this host's partition ID
 #
 
 ROOT_DIR=/home/ec2-user/data0
 
 if [[ $# -lt 4 ]]; then
-  echo "Usage: ./lgendata_scaledsize.sh size_spec num_events num_users split_nums [ keep_only=-1 ]"
-  echo "e.g.: ./lgendata_scaledsize.sh 5m-each 5000000 100000 \"1 2 4 8 16 32\" 1"
+  echo "Usage: ./lgendata_scaledsize.sh size_spec num_events num_users split_nums [ keep_only=-1 ] [ partitioned=false ]"
+  echo "e.g.: ./lgendata_scaledsize.sh 5m-each 5000000 100000 \"1 2 4 8 16 32\" 1 true"
   echo "   keep_only denotes the denotes the *only* output partition that should "
   echo "   actually be saved to disk (-1 for all). should be this host's partition ID"
   echo ""
@@ -30,10 +30,14 @@ if [ $# -ge 5 ]; then
 else
   KEEP_ONLY=-1
 fi
+if [ $# -ge 6 ]; then
+  PARTITIONED=$6
+else
+  PARTITIONED=false
+fi
 
 for i in ${SPLIT_NUMS[*]}; do
   NUM_EVENTS=$(($EVENTS_PER_SPLIT*$i))
   rm -rf $ROOT_DIR/events-$SIZE_SPEC-split-$i
-  java -cp /home/ec2-user/replaydb-worker/replaydb-apps-assembly-0.1-SNAPSHOT.jar replaydb.exec.EventGenerator tunable $NUM_USERS $NUM_EVENTS $ROOT_DIR/events-$SIZE_SPEC-split-$i/events.out $i $KEEP_ONLY
+  java -cp /home/ec2-user/replaydb-worker/replaydb-apps-assembly-0.1-SNAPSHOT.jar replaydb.exec.EventGenerator tunable $NUM_USERS $NUM_EVENTS $ROOT_DIR/events-$SIZE_SPEC-split-$i/events.out $i $KEEP_ONLY $PARTITIONED
 done
-
