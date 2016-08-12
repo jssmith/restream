c0fa99e039bd88a31f83d5949c9ddb5563105c20
Johann Schleier-Smith <jssmith@gmail.com>
2016-08-12 16:35:44 -0700

    change default alpha in lgendata

--- scripts/lgendata_scaledsize.sh
+++ scripts/lgendata_scaledsize.sh
@@ -5,8 +5,8 @@
 ROOT_DIR=/home/ec2-user/data0
 
 if [[ $# -lt 4 ]]; then
-  echo "Usage: ./lgendata_scaledsize.sh size_spec num_events num_users split_nums [ batches=1 ] [ keep_only=-1 ] [ partitioned=false ] [ alpha=1.0 ]"
-  echo "e.g.: ./lgendata_scaledsize.sh 5m-each 5000000 100000 \"1 2 4 8 16 32\" 1 1 true"
+  echo "Usage: ./lgendata_scaledsize.sh size_spec num_events num_users split_nums [ batches=1 ] [ keep_only=-1 ] [ partitioned=false ] [ alpha=2.0 ]"
+  echo "e.g.: ./lgendata_scaledsize.sh 5m-each 5000000 100000 \"1 2 4 8 16 32\" 1 1 true 2.0"
   echo "   keep_only denotes the denotes the *only* output partition that should "
   echo "   actually be saved to disk (-1 for all). should be this host's partition ID"
   echo ""
@@ -48,7 +48,7 @@ fi
 if [ $# -ge 8 ]; then
   ALPHA=$8
 else
-  ALPHA=1.0
+  ALPHA=2.0
 fi
 
 
