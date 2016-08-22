a56615cd92476920ab61b0a68a51b5d241167e9b
Johann Schleier-Smith <jssmith@gmail.com>
2016-08-22 05:08:34 -0700

    add log archive to param sweep

--- scripts/param_sweep_2.sh
+++ scripts/param_sweep_2.sh
@@ -9,11 +9,11 @@
 #
 
 if [[ $# -lt 5 ]]; then
-  echo "Usage: ./param_sweep_2.sh iterations size_spec memsize host_counts detectors [ batch_sizes=10000 ] [ alphas=2.0 ]"
+  echo "Usage: ./param_sweep_2.sh iterations size_spec memsize host_counts detectors [ batch_sizes=10000 ] [ alphas=2.0 ] [ logarchive = s3://edu.berkeley.restream/log ]"
   echo "       where all args except iterations and size_spec should be a space-separated"
   echo "       list of values to sweep over"
   echo "       NOTE: detectors will automatically prepend \"replaydb.exec.spam.\""
-  echo " e.g.: ./param_sweep.sh 10 5m 3000m \"1 2 4 8\" \"SimpleSpamDetectorStats IpSpamDetectorStats\" \"10000 50000\" \"1.75 2.0 2.25\" "
+  echo " e.g.: ./param_sweep.sh 10 5m 3000m \"1 2 4 8\" \"SimpleSpamDetectorStats IpSpamDetectorStats\" \"10000 50000\" \"1.75 2.0 2.25\" s3://edu.berkeley.restream/log"
   exit
 fi
 
@@ -34,6 +34,11 @@ if [ $# -ge 7 ]; then
 else
   alphas='2.0'
 fi
+if [ $# -ge 8 ]; then
+  log_archive=$8
+else
+  log_archive='s3://edu.berkeley.restream/log'
+fi
 
 for iteration in `seq 1 $iterations`; do
   for nhosts in $host_counts; do
@@ -52,7 +57,7 @@ for iteration in `seq 1 $iterations`; do
         fnbase="$UUID-$nhosts-$partitions-$iteration-$detector-$partitioned-$batch_size-$alpha"
         $HOME/replaydb/scripts/ddrive.sh $detector $size_spec-$alpha $partitions $batch_size true $partitioned > $fnbase.txt 2>> $fnbase.timing
         pssh -i -h ~/conf/workers.txt 'gzip log/*.log'
-        pssh -i -h ~/conf/workers.txt "aws s3 sync log s3://edu.berkeley.restream/log/$UUID"
+        pssh -i -h ~/conf/workers.txt "aws s3 sync log $log_archive/$UUID"
       done
       done
     done
